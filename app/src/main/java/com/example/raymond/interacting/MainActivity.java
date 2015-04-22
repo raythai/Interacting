12458baeed2c3553ce96893b27e6da7fe16bf9ad
raythai <raymond.f.thai@gmail.com>
2015-04-22 09:00:59 -0700

    revert test

--- app/src/main/java/com/example/raymond/interacting/MainActivity.java
+++ app/src/main/java/com/example/raymond/interacting/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends ActionBarActivity {
     public void onStayEarthButton(View view) {
         String firstName;
         if(String.valueOf(usersName.getText()).isEmpty()){
-            firstName = "the User";
+            firstName = "the kUser";
         }else {
             firstName = String.valueOf(usersName.getText());
         }
