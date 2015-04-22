2ccfec8a7c8ab83f6cfa4bb041c5d0aeef243d9e
raythai <raymond.f.thai@gmail.com>
2015-04-22 00:10:40 -0700

    added comment for test removed unused imports

--- app/src/main/java/com/example/raymond/interacting/MainActivity.java
+++ app/src/main/java/com/example/raymond/interacting/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.raymond.interacting;
 
-import android.graphics.Color;
-import android.media.MediaPlayer;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
